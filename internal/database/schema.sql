1fed93ac36fa660538a37a008b84505304884fa8
Martins, F. <felipsmartins@gmail.com>
2026-03-07 16:38:29 -0300

    remove unique index.

--- internal/database/schema.sql
+++ internal/database/schema.sql
@@ -8,10 +8,10 @@ CREATE TABLE games
 CREATE TABLE players
 (
     id          INTEGER PRIMARY KEY AUTOINCREMENT,
-    player_id   TEXT UNIQUE NOT NULL,
-    game_id     INT         NOT NULL,
+    player_id   TEXT NOT NULL,
+    game_id     INT  NOT NULL,
     reported_by TEXT,
-    reported_at TEXT        NOT NULL,
+    reported_at TEXT NOT NULL,
     CONSTRAINT fk_name FOREIGN KEY (game_id) REFERENCES games (id)
 );
 
